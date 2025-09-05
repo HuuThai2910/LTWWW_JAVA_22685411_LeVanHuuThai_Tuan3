0dd3fd8c3e23f0240c2d091851763decfc2963d7
HuuThai <levanhuuthai2004@gmail.com>
2025-09-05 23:25:31 +0700

    Hoan thanh bai 2

--- Bai1/src/main/java/fit/iuh/edu/levanhuuthai_22685411_tuan3/Bai1/Gender.java
+++
@@ -1,5 +0,0 @@
-package fit.iuh.edu.levanhuuthai_22685411_tuan3.Bai1;
-
-public enum Gender {
-    MALE, FEMALE
-}
---
+++ Bai2/src/main/java/fit/iuh/edu/levanhuuthai_22685411_tuan3/DBConnection.java
@@ -0,0 +1,13 @@
+/*
+ * @ (#) .java    1.0       
+ * Copyright (c)  IUH. All rights reserved.
+ */
+package fit.iuh.edu.levanhuuthai_22685411_tuan3;
+/*
+ * @description
+ * @author: Huu Thai
+ * @date:   
+ * @version: 1.0
+ */
+public class DBConnection {
+}
